392f2f9b1fcecbabdeebb06699b69389770739df
TimL2 <108150020+TimL2@users.noreply.github.com>
2022-07-17 16:28:35 -0500

    revised main.py in PyBank with error fixes, now shows correct solution

--- Starter_Code/PyBank/.ipynb_checkpoints/main-checkpoint.py
+++ Starter_Code/PyBank/.ipynb_checkpoints/main-checkpoint.py
@@ -10,15 +10,20 @@ csvpath = Path('Resources/budget_data.csv')
 line_num = 0
 profits = []
 dates = []
+changes = []
 
 #Initialize math variables
 total_months = 0
 total_profit = 0
 average_profit = 0
+average_change = 0
+total_change = 0
+change_count = 0
 max_profit = 0
 max_profit_date = ""
 min_profit = 0
 min_profit_date = ""
+prev_profit = 0
 
 # Open the input path as a file object
 with open(csvpath, 'r') as csvfile:
@@ -40,7 +45,12 @@ with open(csvpath, 'r') as csvfile:
         profits.append(profit)
         date = str(row[0])
         dates.append(date)
-        
+        change = profit - prev_profit
+        if csvreader.line_num > 2:
+            changes.append(change)
+            total_change += change
+            change_count += 1
+            
         #Add 1 to month count
         total_months += 1
         
@@ -48,18 +58,19 @@ with open(csvpath, 'r') as csvfile:
         total_profit += profit
         
         #Check for max profit and min profit
-        if profit > max_profit:
-            max_profit = profit
+        if change > max_profit:
+            max_profit = change
             max_profit_date = date
         elif min_profit == 0:
-            min_profit = profit
+            min_profit = change
             min_profit_date = date
-        elif profit < min_profit:
-            min_profit = profit
+        elif change < min_profit:
+            min_profit = change
             in_profit_date = date
-            
+        prev_profit = profit
+        
     #Calc average profit
-    average_profit = (total_profit/total_months)
+    average_profit = (total_change / change_count)
 
 #Display Results
 print()
@@ -93,3 +104,4 @@ with open(output_path, 'w') as csvfile:
     csvwriter.writerow(header)
     # Write the list of metrics to the output file
     csvwriter.writerow(metrics)
+
